2a4e4163e0652be1ee0417072556a4a262ccdd94
iamzjt-front-end <iamzjt@foxmail.com>
2020-08-04 16:40:47 +0800

    添加 icon 和 iconPosition

--- src/button.vue
+++ src/button.vue
@@ -1,10 +1,17 @@
 <template>
-  <button class="z-button">按钮</button>
+  <button class="z-button" :class="{[`icon-${iconPosition}`]: true}">
+    <svg v-if="icon" class="icon">
+      <use :xlink:href="`#i-${icon}`"></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 
 <script>
   export default {
-
+    props: ['icon', 'iconPosition']
   }
 </script>
 
@@ -16,14 +23,43 @@
     border-radius: var(--button-radius);
     border: 1px solid var(--border-color);
     background: var(--button-bg);
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    vertical-align: middle;
+
     &:hover {
       border-color: var(--border-color-hover);
     }
+
     &:active {
       background-color: var(--button-active-bg);
     }
+
     &:focus {
       outline: none;
+    }
+
+    > .icon {
+      order: 1;
+      margin-right: .15em;
+      margin-top: .125em;
+    }
+
+    > .content {
+      order: 2;
+    }
+
+    &.icon-right {
+      > .icon {
+        order: 2;
+        margin-right: 0;
+        margin-left: .15em;
+        margin-top: .125em;
+      }
+      > .content {
+        order: 1;
+      }
     }
   }
 </style>
