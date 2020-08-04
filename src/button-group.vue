fd1285c87a5e69513f37d536f71385b3919c357a
iamzjt-front-end <iamzjt@foxmail.com>
2020-08-04 18:35:33 +0800

    更新 loading 图标

--- src/button-group.vue
+++ src/button-group.vue
@@ -10,7 +10,7 @@
     mounted() {
       for (let node of this.$el.children) {
         let name = node.nodeName.toLowerCase()
-        if(name != 'button') {
+        if (name != 'button') {
           console.warn(`z-button-group 的子元素应该全是 z-button, 不能写 ${name}`)
         }
       }
@@ -22,17 +22,23 @@
   .z-button-group {
     display: inline-flex;
     vertical-align: middle;
+
     > .z-button {
       border-radius: 0;
+      &:not(:first-child) {
       margin-left: -1px;
+    }
+
       &:first-child {
         border-top-left-radius: var(--button-radius);
         border-bottom-left-radius: var(--button-radius);
       }
+
       &:last-child {
         border-top-right-radius: var(--button-radius);
         border-bottom-right-radius: var(--button-radius);
       }
+
       &:hover {
         z-index: 1;
       }
