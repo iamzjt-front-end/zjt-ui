3b0a0d87243b828a7e17e6b2664a28ea52fc0ded
iamzjt-front-end <iamzjt@foxmail.com>
2020-08-08 21:51:34 +0800

    添加 single（可选单开/多开） 选项

--- src/z-collapse-item.vue
+++ src/z-collapse-item.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="z-collapse-item">
-    <div class="title" @click="open=!open">
+    <div class="title" @click="toggle">
       {{title}}
     </div>
     <div class="content" v-if="open">
@@ -22,6 +22,27 @@ export default {
     return {
       open: false
     }
+  },
+  inject: ['eventBus'],
+  mounted() {
+    this.eventBus && this.eventBus.$on('update:selected', vm => {
+      if(vm !== this) {
+        this.close()
+      }
+    })
+  },
+  methods: {
+    close() {
+      this.open = false
+    },
+    toggle() {
+      if(this.open) {
+        this.open = false
+      } else {
+        this.open = true
+        this.eventBus && this.eventBus.$emit('update:selected', this)
+      }
+    },
   }
 }
 </script>
@@ -36,6 +57,7 @@ $border-radius: 4px;
     display: flex;
     align-items: center;
     padding: 0 8px;
+    cursor: pointer;
   }
 
   > .content {
