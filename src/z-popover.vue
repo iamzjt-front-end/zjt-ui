5a76ae31f71d536eec0c730a6b4807f2e9a65a66
iamzjt-front-end <iamzjt@foxmail.com>
2020-08-08 14:33:36 +0800

    完善 popover 基本功能

--- src/z-popover.vue
+++ src/z-popover.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="z-popover" @click.stop="toggle">
+  <div class="z-popover" @click.stop="toggle" ref="popover">
     <div ref="contentWrapper" class="content-wrapper" v-if="visible">
       <slot name="content"></slot>
     </div>
@@ -18,24 +18,40 @@ export default {
     }
   },
   methods: {
-    toggle() {
-      this.visible = !this.visible
-      if (this.visible === true) {
-        this.$nextTick(() => {
-          document.body.appendChild(this.$refs.contentWrapper)
-          let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
-          this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
-          this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
-          let eventHandler = () => {
-            this.visible = false
-            document.removeEventListener('click', eventHandler)
-          }
-          document.addEventListener('click', eventHandler)
-        })
+    positionContent() {
+      document.body.appendChild(this.$refs.contentWrapper)
+      let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
+      this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+      this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+    },
+    onClickDocument(e) {
+      if (this.$refs.popover &&
+          (this.$refs.popover === e.target || this.$refs.popover.contains(e.target))
+      ) {
+        return
       }
-    }
-  },
-  mounted() {
+      this.close()
+    },
+    open() {
+      this.visible = true
+      this.$nextTick(() => {
+        this.positionContent()
+        document.addEventListener('click', this.onClickDocument)
+      })
+    },
+    close() {
+      this.visible = false
+      document.removeEventListener('click', this.onClickDocument)
+    },
+    toggle(event) {
+      if (this.$refs.triggerWrapper.contains(event.target)) {
+        if (this.visible === true) {
+          this.close()
+        } else {
+          this.open()
+        }
+      }
+    },
   }
 }
 </script>
